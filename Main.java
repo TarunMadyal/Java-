da88db93f4b4cee0a42b470ac23ac49ce9afe334
Tarun <143196358+TarunMadyal@users.noreply.github.com>
2024-07-25 16:56:36 +0530

    Create Main.java
    Building 
    *
    **
    ***
    ****
    *****
    Pattern

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+        for(int i = 0; i<=4;i++){
+          for (int j=i;j>=1;j--){
+            System.out.print("*");
+          }
+          System.out.println("*");
+        }
+    }
+}
+     
