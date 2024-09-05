a1358a9eebcebfaec7130bd7e5591861cf09f6eb
Tarun <143196358+TarunMadyal@users.noreply.github.com>
2024-09-05 16:30:52 +0530

    Create pyramid.java

---
+++ pyramid.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++){
+            for(int j=0; j < 5-i-1; j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<i*2-1;j++){
+                System.out.print("*");
+            }
+            for(int j=0; j < 5-i-1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
