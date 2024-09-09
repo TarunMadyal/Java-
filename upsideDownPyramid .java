49fb7066409c25a388c38ae07ab8a9ed9bd146ad
Tarun <143196358+TarunMadyal@users.noreply.github.com>
2024-09-09 19:18:38 +0530

    Create upsideDownPyramid .java

---
+++ upsideDownPyramid .java
@@ -0,0 +1,17 @@
+public class Tom{
+    public static void main(String[] args){
+        int n= 7;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j < 2*n-(2*i+1); j++){
+                System.out.print("*");
+            }
+            for(int j = 0; j < i; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+         }
+    }
+}
